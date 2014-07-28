1cc217234526cb83b12802ed2f35edfa9fc9c481
soudamini-patil <soudamini.patil@cumminscollege.in>
2014-07-28 11:34:29 +0530

    Update calc.cpp
    added mul() method

--- calc.cpp
+++ calc.cpp
@@ -10,3 +10,8 @@ double add (double in_number1, double in_number2)
 {
   return in_number1 + in_number2;
 }
+
+double mul (double in_number1, double in_number2)
+{
+  return in_number1 * in_number2;
+}
