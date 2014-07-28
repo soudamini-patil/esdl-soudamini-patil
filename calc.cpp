601b47e5b72bde1ae4c3dc423adb4ea71ba255f0
soudamini-patil <soudamini.patil@cumminscollege.in>
2014-07-28 11:27:31 +0530

    Update calc.cpp
    added sub() method

--- calc.cpp
+++ calc.cpp
@@ -10,3 +10,9 @@ double add (double in_number1, double in_number2)
 {
   return in_number1 + in_number2;
 }
+
+
+double sub (double in_number1, double in_number2)
+{
+  return in_number1 -  in_number2;
+}
